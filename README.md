b6e2440505b4afe5a69f1a9ba17e284cd2be5941
justGYF <justgy_f@163.com>
2020-09-17 23:31:21 +0800

    flatten

--- README.md
+++ README.md
@@ -30,3 +30,6 @@
 ## NODE
 
 ## TYPESCRIPT
+
+
+
---
+++ code/js/flattenArr.js
@@ -0,0 +1,13 @@
+// 数组扁平化
+
+const arr = [1, 2, 3, ['a', 'b'], ['c', 1, [2, 3, ['e', ['f']]]]]
+
+
+const flatten = (arr) => {
+    return arr.reduce((a,b) => {
+        return a.concat(b instanceof Array ? flatten(b) : b)
+    }, [])
+}
+
+let newArr = flatten(arr)
+console.log(newArr)

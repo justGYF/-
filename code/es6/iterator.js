97ad11084223e8b59132296b1813657a6aee4149
justGYF <justgy_f@163.com>
2021-12-15 00:12:59 +0800

    Symbol.iterator

--- code/es6/iterator.js
+++ code/es6/iterator.js
@@ -1,9 +1,48 @@
 // Iterator 遍历器  统一的接口机制，处理所有不同的数据结构
 // 任何数据结构只要部署Iterator接口，就可以完成遍历操作
+// es6 默认一个数据结构只要具备Symbol.iterator属性，就认为是可以迭代的
 
 /**
  * Iterator 的作用
  * 1. 为各种数据结构提供统一的访问接口
  * 2. 使数据结构的成员可以按某种顺序排列
  * 3. 可以使用 for...of..
+ * 
+ * 默认具备 Symbol.iterator属性的数据结构
+ * 1. Array
+ * 2. Map
+ * 3. Set
+ * 4. String
+ * 5. TypedArray
+ * 6. 函数的arguments对象
+ * 7. NodeList对象
 */
+
+// Array.prototype[Symbol.iterator] = () => {
+
+// }
+
+
+// 为原生的Object添加 Iterator
+let obj = {
+    a: 1111,
+    b: 2222,
+    [Symbol.iterator] () {
+        let arr = Object.keys(this).map(it => this[it])
+        let len = arr.length
+        let start = 0
+        return {
+            next() {
+                if (start >= len) {
+                    return {value: undefined, done: true}
+                } else {
+                    return {value: arr[start++], done: false}
+                }
+            }
+        }
+    }
+
+}
+for (let a of obj) {
+    console.log(a)
+}
